8fb461a7bdc5df58e003e9d31f1479ec1c3730f1
German Bonin <germanbonin@gmail.com>
2021-09-09 18:22:23 -0300

    add: template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Maintenance Alert</h1>
+      <p>
+        <br>We're starting a scheduled downtime on the Fleek app  to carry out a maintenance upgrade</br>
+        <br>The expected duration of this downtime is 1 hour:alarm_clock: During that time, the app/platform won't be available.</br>
+        <br>Sites & content hosted on Fleek won't be affected </br>
+      </p>
     </div>
   );
 }
