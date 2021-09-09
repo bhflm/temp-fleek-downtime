2522a811a9cc8967cadcf933bdc8f454b3e3128d
German Bonin <germanbonin@gmail.com>
2021-09-09 18:24:11 -0300

    fix: elements

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ function App() {
   return (
     <div className="App">
     <h1>Maintenance Alert</h1>
-      <p>
-        <br>We're starting a scheduled downtime on the Fleek app  to carry out a maintenance upgrade</br>
-        <br>The expected duration of this downtime is 1 hour:alarm_clock: During that time, the app/platform won't be available.</br>
-        <br>Sites & content hosted on Fleek won't be affected </br>
-      </p>
+      <div>
+        <p>We're starting a scheduled downtime on the Fleek app  to carry out a maintenance upgrade</p>
+        <p>The expected duration of this downtime is 1 hour </p>
+        <p>During that time, the app/platform won't be available.</p>
+        <p>Sites & content hosted on Fleek won't be affected </p>
+      </div>
     </div>
   );
 }
