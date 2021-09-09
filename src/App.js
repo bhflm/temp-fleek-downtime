388891e6221eb56362791fb904166102d6c48547
German Bonin <germanbonin@gmail.com>
2021-09-09 18:30:25 -0300

    add: template

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Maintenance Alert</h1>
+      <div>
+        <p>We're starting a scheduled downtime on the Fleek app  to carry out a maintenance upgrade</p>
+        <p>The expected duration of this downtime is 1 hour </p>
+        <p>During that time, the app/platform won't be available.</p>
+        <p>Sites & content hosted on Fleek won't be affected </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
